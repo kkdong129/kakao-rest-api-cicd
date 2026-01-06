b94e4c69234faca1e7e05eae4d6582b858609009
kkdong129 <31696828+kkdong129@users.noreply.github.com>
2026-01-06 20:46:46 +0900

    Update refresh_kakao.py

--- refresh_kakao.py
+++ refresh_kakao.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import datetime
 
 # 1. GitHub Secrets에서 정보 가져오기
 raw_data = {
@@ -30,7 +31,7 @@ client_id = raw_data["CLIENT_ID"]
 client_secret = raw_data["CLIENT_SECRET"]
 refresh_token = raw_data["REFRESH_TOKEN"]
 
-def send_kakao_feed_message(access_token, is_success=True, error_msg=""):
+def send_kakao_feed_message(access_token, is_success=True, expires_msg="", error_msg=""):
     """나에게 카카오톡 메시지 보내기 (피드 템플릿 사용)"""
     url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
     headers = {
@@ -39,7 +40,7 @@ def send_kakao_feed_message(access_token, is_success=True, error_msg=""):
     }
 
     title = f"Github actions 작업 결과"
-    description = "카카오 토큰 갱신이 정상적으로 완료되었습니다." if is_success else f"에러 발생: {error_msg}"
+    description = "카카오 토큰 갱신이 정상적으로 완료되었습니다.\n리프레시 토큰 남은 유효기간(일): {expires_msg}" if is_success else f"에러 발생: {error_msg}"
 
     # 피드(Feed) 템플릿 구조
     template_object = {
@@ -75,7 +76,7 @@ def send_kakao_feed_message(access_token, is_success=True, error_msg=""):
     else:
         print(f"카톡 알림 전송 실패! 상태코드: {response.status_code}, 상세내용: {response.json()}")
 
-def send_kakao_text_message(access_token, is_success=True, error_msg=""):
+def send_kakao_text_message(access_token, is_success=True, expires_msg= "", error_msg=""):
     """나에게 카카오톡 메시지 보내기 (성공/실패 공용)"""
     url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
     headers = {
@@ -85,7 +86,7 @@ def send_kakao_text_message(access_token, is_success=True, error_msg=""):
 
     # 상태에 따른 메시지 내용 분기
     if is_success:
-        main_text = "Github actions 작업 결과: 카카오 토큰 갱신이 정상적으로 완료되었습니다."
+        main_text = f"Github actions 작업 결과: 카카오 토큰 갱신이 정상적으로 완료되었습니다.\n리프레시 토큰 남은 유효기간(일): {expires_msg}"
     else:
         main_text = f"Github actions 작업 결과: 에러 발생\n사유: {error_msg}"
 
@@ -121,11 +122,23 @@ def refresh_access_token():
 
     if response.status_code == 200:
         new_access_token = result.get('access_token')
+        remained_seconds = result.get('refresh_token_expires_in')
+
+        # 리프레시 토큰 만료일 확인 로직
+        if remained_seconds:
+            # 초 단위를 '일' 단위로 환산
+            remained_days = remained_seconds / (60 * 60 * 24)
+            print(f"알림: 리프레시 토큰의 유효기간이 약 {remained_days:.1f}일 남았습니다.")
+
+            if remained_days < 7:
+                print("경고: 리프레시 토큰 만료가 7일 미만입니다! 곧 업데이트가 필요합니다.")
+
+        expires_msg = remained_days
         print("새로운 액세스 토큰 발급 성공!")
         print(f"new_access_token: {new_access_token[:5]}...{new_access_token[-5:]}")
         # 성공 메시지 전송
-        # send_kakao_feed_message(new_access_token, is_success=True) # 피드 템플릿
-        send_kakao_text_message(new_access_token, is_success=True) # 텍스트 템플릿
+        # send_kakao_feed_message(new_access_token, is_success=True, expires_msg) # 피드 템플릿
+        send_kakao_text_message(new_access_token, is_success=True, expires_msg) # 텍스트 템플릿
     else:
         error_info = result.get('error_description', 'Unknown Error')
         print(f"토큰 갱신 실패: {error_info}")
